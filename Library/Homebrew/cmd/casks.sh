11631769cd3faf9d1d337f143755c61dfc4e4442
rcmdnk <rcmdnk@gmail.com>
2023-02-18 08:47:00 +0900

    Show casks from API as same as formulae

--- Library/Homebrew/cmd/casks.sh
+++ Library/Homebrew/cmd/casks.sh
@@ -8,5 +8,18 @@
 source "${HOMEBREW_LIBRARY}/Homebrew/items.sh"
 
 homebrew-casks() {
-  homebrew-items '*/Casks/*\.rb' '' 's|/Casks/|/|' '^homebrew/cask'
+  local casks
+  casks="$(homebrew-items '*/Casks/*\.rb' '' 's|/Casks/|/|' '^homebrew/cask')"
+
+  # HOMEBREW_CACHE is set by brew.sh
+  # shellcheck disable=SC2154
+  if [[ -z "${HOMEBREW_NO_INSTALL_FROM_API}" &&
+        -f "${HOMEBREW_CACHE}/api/cask.json" ]]
+  then
+    local api_casks
+    api_casks="$(ruby -e "require 'json'; JSON.parse(File.read('${HOMEBREW_CACHE}/api/cask.json')).each { |f| puts f['token'] }" 2>/dev/null)"
+    casks="$(echo -e "${casks}\n${api_casks}" | sort -uf | grep .)"
+  fi
+
+  echo "${casks}"
 }
