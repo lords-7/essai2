8db9e008603e52e3ad3bc8c73fe2cd542bebdbdf
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-25 18:43:37 -0400

    tests: rewrite

--- Library/Homebrew/test/dev-cmd/bump-cask-pr_spec.rb
+++ Library/Homebrew/test/dev-cmd/bump-cask-pr_spec.rb
@@ -5,16 +5,33 @@ require "dev-cmd/bump-cask-pr"
 
 describe "brew bump-cask-pr" do
   it_behaves_like "parseable arguments"
+
   describe Homebrew::NewVersion do
     let(:general_version) { "1.2.3" }
     let(:intel_version) { "2.3.4" }
     let(:arm_version) { "3.4.5" }
 
-    describe "#initialize" do
-      context "when only the general version is provided" do
-        it "parses the version and not raise an error" do
-          expect { described_class.new(general: general_version) }.not_to raise_error
-        end
+    context "when initializing with no versions" do
+      it "raises a usage error" do
+        expect do
+          described_class.new
+        end.to raise_error(UsageError, "Invalid usage: `--version` must not be empty.")
+      end
+    end
+
+    context "when initializing with valid versions" do
+      let(:new_version) { described_class.new(general: general_version, arm: arm_version, intel: intel_version) }
+
+      it "correctly parses general version" do
+        expect(new_version.general).to eq(Cask::DSL::Version.new(general_version.to_s))
+      end
+
+      it "correctly parses arm version" do
+        expect(new_version.arm).to eq(Cask::DSL::Version.new(arm_version.to_s))
+      end
+
+      it "correctly parses intel version" do
+        expect(new_version.intel).to eq(Cask::DSL::Version.new(intel_version.to_s))
       end
 
       context "when only the intel version is provided" do
@@ -35,44 +52,24 @@ describe "brew bump-cask-pr" do
         end
       end
 
-      context "when the general version and intel version are both provided" do
-        it "raises a UsageError" do
-          expect { described_class.new(general: general_version, intel: intel_version) }
-            .to raise_error(UsageError,
-                            "Invalid usage: You cannot specify --version with --version-intel and --version-arm.")
-        end
-      end
-
-      context "when all versions are provided" do
-        it "raises a UsageError" do
-          expect { described_class.new(general: general_version, intel: intel_version, arm: arm_version) }
-            .to raise_error(UsageError,
-                            "Invalid usage: You cannot specify --version with --version-intel and --version-arm.")
-        end
-      end
-    end
-
-    describe "#parse_new_version" do
       context "when the version is latest" do
         it "returns a version object for latest" do
           new_version = described_class.new(general: "latest")
           expect(new_version.general.to_s).to eq("latest")
         end
-      end
 
-      context "when the version is not latest" do
-        it "returns a version object for the given version" do
-          new_version = described_class.new(general: general_version)
-          expect(new_version.general.to_s).to eq(general_version)
+        context "when the version is not latest" do
+          it "returns a version object for the given version" do
+            new_version = described_class.new(general: general_version)
+            expect(new_version.general.to_s).to eq(general_version)
+          end
         end
       end
-    end
 
-    describe "#blank?" do
-      context "when a version is given" do
-        it "returns false" do
-          new_version = described_class.new(general: general_version)
-          expect(new_version.blank?).to be false
+      context "when checking if NewVersion is blank" do
+        it "returns false if any version is present" do
+          new_version = described_class.new(general: general_version.to_s, arm: "", intel: "")
+          expect(new_version.blank?).to be(false)
         end
       end
     end
