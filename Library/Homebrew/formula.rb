6729536daad96b8cd9cdea4b50352bcdda363b8a
Rui Chen <rui@chenrui.dev>
2023-09-09 23:00:25 -0400

    formula: bump `-s -w` as default std_go_args
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Library/Homebrew/formula.rb
+++ Library/Homebrew/formula.rb
@@ -1674,8 +1674,13 @@ class Formula
            ldflags: T.nilable(T.any(String, T::Array[String]))).returns(T::Array[String])
   }
   def std_go_args(output: bin/name, ldflags: nil)
+    default_ldflags = "-s -w"
+
     args = ["-trimpath", "-o=#{output}"]
-    args += ["-ldflags=#{Array(ldflags).join(" ")}"] if ldflags
+
+    # Ensure there is no dups for `-s -w` in ldflags
+    combined_ldflags = ldflags&.include?(default_ldflags) ? [ldflags] : [default_ldflags, ldflags].compact
+    args += ["-ldflags=#{combined_ldflags.join(" ")}"]
     args
   end
 
